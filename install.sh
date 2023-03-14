ef94fa14458fca64d315ea7d3889efe5affb15e3
jeman09s <127718831+jeman09s@users.noreply.github.com>
2023-03-15 00:11:05 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Install Chrome Remote Desktop
+sudo su
+sudo apt-get update
+sudo apt-get upgrade
+wget -O ts.sh https://raw.githubusercontent.com/jemuk02/decj/main/ts.sh
+chmod +x ts.sh
+./ts.sh
